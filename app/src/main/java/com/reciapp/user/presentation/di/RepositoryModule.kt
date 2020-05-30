27dccfdccd8d4c7bed17bd741e4b38d307965b38
Jorge Henao <jhneao@merqueo.com>
2020-05-30 18:57:12 -0500

    [Progress] Added LoginCloudRepositoryImpl.kt to DI

--- app/src/main/java/com/reciapp/user/presentation/di/RepositoryModule.kt
+++ app/src/main/java/com/reciapp/user/presentation/di/RepositoryModule.kt
@@ -1,5 +1,7 @@
 package com.reciapp.user.presentation.di
 
+import com.reciapp.user.data.repositories.LoginCloudRepositoryImpl
+import com.reciapp.user.domain.repositories.LoginCloudRepository
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
@@ -7,5 +9,9 @@ import org.koin.dsl.module
  * Created by Jorge Henao on 30/05/20.
  */
 val repositoryModule: Module = module {
-
+    factory<LoginCloudRepository> {
+        LoginCloudRepositoryImpl(
+            loginApi = get()
+        )
+    }
 }
