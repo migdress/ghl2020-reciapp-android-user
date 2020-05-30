255faaf19678ebb688152d40c89f5a02da282f80
Jorge Henao <jhneao@merqueo.com>
2020-05-30 14:27:25 -0500

    [Progress] Join login_nav_graph with LoginActivity.kt

--- app/src/main/java/com/reciapp/user/presentation/views/activities/LoginActivity.kt
+++ app/src/main/java/com/reciapp/user/presentation/views/activities/LoginActivity.kt
@@ -2,11 +2,19 @@ package com.reciapp.user.presentation.views.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import com.reciapp.user.R
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        initView()
+    }
+
+    private fun initView() {
+        findNavController(R.id.fraNavigationLogin)
+            .setGraph(R.navigation.login_nav_graph, intent.extras)
     }
 }
