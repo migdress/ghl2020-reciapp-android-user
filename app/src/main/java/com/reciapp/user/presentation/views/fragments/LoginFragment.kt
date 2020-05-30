7f82b5f6cb2936526d6bcc47d855d8b45a1f14ad
Jorge Henao <jhneao@merqueo.com>
2020-05-30 18:08:11 -0500

    [Progress] Validate empty fields

--- app/src/main/java/com/reciapp/user/presentation/views/fragments/LoginFragment.kt
+++ app/src/main/java/com/reciapp/user/presentation/views/fragments/LoginFragment.kt
@@ -1,16 +1,38 @@
 package com.reciapp.user.presentation.views.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.reciapp.user.R
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private val textWatcher: TextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(
+            charSequence: CharSequence,
+            i: Int,
+            i2: Int,
+            i3: Int
+        ) {
+        }
+
+        override fun onTextChanged(
+            charSequence: CharSequence,
+            i: Int,
+            i2: Int,
+            i3: Int
+        ) {
+        }
+
+        override fun afterTextChanged(editable: Editable) {
+            // check Fields For Empty Values
+            checkFieldsForEmptyValues()
+        }
     }
 
     override fun onCreateView(
@@ -19,5 +41,22 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initListeners()
+    }
+
+    private fun initListeners() {
+        tieUserName.addTextChangedListener(textWatcher)
+        tieUserPassword.addTextChangedListener(textWatcher)
+    }
+
+    fun checkFieldsForEmptyValues() {
+        val user: String = tieUserName.text.toString()
+        val password: String = tieUserPassword.text.toString()
+        btnLogin.isEnabled = !(user.isEmpty() || password.isEmpty())
     }
 }
