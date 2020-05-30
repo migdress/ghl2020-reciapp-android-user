2f4cdab3a0c646e0064a3d39eca385f5cbcb3465
Jorge Henao <jhneao@merqueo.com>
2020-05-30 17:07:28 -0500

    [Progress] Clean imports

--- app/src/main/java/com/reciapp/user/presentation/views/fragments/SplashFragment.kt
+++ app/src/main/java/com/reciapp/user/presentation/views/fragments/SplashFragment.kt
@@ -1,13 +1,10 @@
 package com.reciapp.user.presentation.views.fragments
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.reciapp.user.R
 
