25922fbfd3f64ea8038c72ce4ab490886c5f9243
Jorge Henao <jhneao@merqueo.com>
2020-05-30 18:55:19 -0500

    [Progress] Added logic to LoginCloudRepositoryImpl.kt

--- app/src/main/java/com/reciapp/user/data/repositories/LoginCloudRepositoryImpl.kt
+++ app/src/main/java/com/reciapp/user/data/repositories/LoginCloudRepositoryImpl.kt
@@ -1,9 +1,18 @@
 package com.reciapp.user.data.repositories
 
+import com.reciapp.user.data.models.login.LoginRequest
+import com.reciapp.user.data.networkeEndPoints.LoginApi
 import com.reciapp.user.domain.repositories.LoginCloudRepository
 
 /**
  * Created by Jorge Henao on 30/05/20.
  */
-class LoginCloudRepositoryImpl: LoginCloudRepository {
+class LoginCloudRepositoryImpl(
+    private val loginApi: LoginApi
+) : LoginCloudRepository {
+
+    /**
+     * Method used to generate the login request
+     */
+    override fun login(loginRequest: LoginRequest) = loginApi.login(loginRequest)
 }
--- app/src/main/java/com/reciapp/user/domain/repositories/LoginCloudRepository.kt
+++ app/src/main/java/com/reciapp/user/domain/repositories/LoginCloudRepository.kt
@@ -1,7 +1,16 @@
 package com.reciapp.user.domain.repositories
 
+import com.reciapp.user.data.models.login.LoginRequest
+import com.reciapp.user.data.models.login.LoginResponse
+import io.reactivex.Single
+
 /**
  * Created by Jorge Henao on 30/05/20.
  */
 interface LoginCloudRepository {
+
+    /**
+     * Method used to generate the login request
+     */
+    fun login(loginRequest: LoginRequest): Single<LoginResponse>
 }
