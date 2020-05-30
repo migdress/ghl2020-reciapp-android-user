5a073523ae1fc5b5eb82d06cb2e52f3ad97cb55c
Jorge Henao <jhneao@merqueo.com>
2020-05-30 15:08:38 -0500

    [Progress] fragment_splash.xml was created

--- app/src/main/java/com/reciapp/user/presentation/views/activities/LoginActivity.kt
+++ app/src/main/java/com/reciapp/user/presentation/views/activities/LoginActivity.kt
@@ -1,7 +1,7 @@
 package com.reciapp.user.presentation.views.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import com.reciapp.user.R
 
