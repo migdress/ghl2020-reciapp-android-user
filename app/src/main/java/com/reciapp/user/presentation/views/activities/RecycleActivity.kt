0d7002cc580c92e5ea8fcddd879062299f262360
Jorge Henao <jhneao@merqueo.com>
2020-05-31 08:17:18 -0500

    [Progress] Clean RecycleActivity.kt

--- app/src/main/java/com/reciapp/user/presentation/views/activities/RecycleActivity.kt
+++ app/src/main/java/com/reciapp/user/presentation/views/activities/RecycleActivity.kt
@@ -14,6 +14,8 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.reciapp.user.R
+import kotlinx.android.synthetic.main.activity_recycle.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
 class RecycleActivity : AppCompatActivity() {
 
@@ -22,14 +24,21 @@ class RecycleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycle)
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
+
+        setToolbar()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun setToolbar() {
+        toolbar.title = ""
         setSupportActionBar(toolbar)
 
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home, R.id.nav_gallery
@@ -37,10 +46,5 @@ class RecycleActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
